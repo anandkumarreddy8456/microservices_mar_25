bac29c6e46e0e8cf1f29a513f7633f5a464e7ba4
anand_reddy <anandkumarreddy8456@gmail.com>
2025-03-17 22:19:11 +0530

    Category Service has Updated

--- Category-Services/src/main/java/com/anand/category/repository/CategoryRepository.java
+++ Category-Services/src/main/java/com/anand/category/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.anand.category.repository;
 import com.anand.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+    Set<Category> findByOnlineAppId(Long id);
 }
